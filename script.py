7f203c85aaf45359eb124240e10cfcc92589490b
SalmaFetji <salma_fetji@hotmail.fr>
2021-08-11 08:23:53 +0200

    modf Salma

--- script.py
+++ script.py
@@ -1,37 +1,34 @@
-uri = 'https://swapi.dev/api/planets'
+url_planets = 'https://swapi.dev/api/planets/'
+def fetch_num_planets ():
+    r = requests.get(url_planets)
+    if r.status_code == 200:
+        data = r.json()
+        count = data["count"]
+        return count
+    else:
+        return "NO DATA"
+fetch_num_planets()
 
-n_planets = 10
+def fetch_all_planets():
+    datalist=[]
+    for i in range (1, fetch_num_planets()+2):
+        url = f'{url_planets}{str(i)}'
+        r_people = requests.get(url)
+        if r_people.status_code == 200:
+            data_people = r_people.json()
+            datalist.append(data_people)
+    return datalist
+fetch_all_planets()
 
-data = []
-for i in range(1, n_planets):
-    response = requests.get(f'{uri}/{str(i)}')
-    if response.status_code == 200:
-        data.append(response.json())
-    else: 
-        data = []
-        
-
-columns = ['name','rotation_period','orbital_period', "diameter", "climate", "gravity", "terrain", "surface_water", "population", "residents", "films", "created", "edited","url"]
-planets_df = pd.DataFrame(columns=columns)
-planets_df
-
-data
-
-for index, row in enumerate(data):
-    
-    planets_df.loc[index,'name'] = row['name']
-    planets_df.loc[index,'rotation_period'] = row['rotation_period']
-    planets_df.loc[index,'orbital_period'] = row['orbital_period']
-    planets_df.loc[index,'diameter'] = row['diameter']
-    planets_df.loc[index,'climate'] = row['climate']
-    planets_df.loc[index,'gravity'] = row['gravity']
-    planets_df.loc[index,'terrain'] = row['terrain']
-    planets_df.loc[index,'surface_water'] = row['surface_water']
-    planets_df.loc[index,'population'] = row['population']
-    planets_df.loc[index,'residents'] = row['residents']
-    planets_df.loc[index,'films'] = row['films']
-    planets_df.loc[index,'created'] = row['created']
-    planets_df.loc[index,'edited'] = row['edited']
-    planets_df.loc[index,'url'] = row['url']
-    
-planets_df
+def planets_dframe():
+    columns = ["name","climate","terrain","population","residents","films"]
+    planets_df= pd.DataFrame(columns=columns)
+    for index, row in enumerate(fetch_all_planets()):
+        planets_df.loc[index,'name']=row['name']
+        planets_df.loc[index,'climate']=row['climate']
+        planets_df.loc[index,'terrain']=row['terrain']
+        planets_df.loc[index,'population']=row['population']
+        planets_df.loc[index,'residents']=row['residents']
+        planets_df.loc[index,'films']=row['films']
+    return planets_df
+#print(planets_dframe())
